561b78e858d521e0665ec63ddacdc3891bf718be
rarosario <rafael.rosario@mercadolivre.com>
2021-09-16 23:34:56 -0300

    Atualização do Palindromo.

--- src/com/company/Exercicio2/ManipulaString.java
+++ src/com/company/Exercicio2/ManipulaString.java
@@ -9,6 +9,7 @@ public class ManipulaString {
     public static void main(String[] args) {
         String nome;
         Scanner tecladoReverse = new Scanner(System.in);
+        System.out.println("------Reverse");
         System.out.println(" Digite seu nome ? ");
         nome = tecladoReverse.next();
         inverteString(nome);
@@ -20,10 +21,21 @@ public class ManipulaString {
                 resultado += nome.charAt(x);
             }
             System.out.println(resultado.toUpperCase(Locale.ROOT));
-
         }
 
         public static void palindromo (String nome){
 
+            String texto;
+            String aux;
+            System.out.println("Digite a Palavra em maiusculo ou minusculo!" );
+            Scanner tecladoReverse = new Scanner(System.in);
+            texto = tecladoReverse.next();
+            System.out.print("A Palavra: " +texto);
+            aux = new StringBuffer(texto).reverse().toString();
+            if (texto.equals(aux)) {
+                System.out.println("  é palindromo");
+            } else {
+                System.out.println(" Não é Palindromo");
+            }
         }
     }
